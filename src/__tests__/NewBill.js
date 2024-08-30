f8bf97b83043cb28bf49d11437deb36d4d545850
enzolp28 <laposta.enzo@gmail.com>
2024-08-30 14:38:35 +0200

    test newBill.js  optimiser 2

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -12,6 +12,7 @@ import router from "../app/Router.js";
 import { fireEvent } from "@testing-library/dom";
 import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
+
 const onNavigate = (pathname) => {
   document.body.innerHTML = ROUTES({ pathname })
 }
@@ -56,11 +57,52 @@ describe("Given I am connected as an employee", () => {
       //   document.body.innerHTML = ROUTES({ pathname })
       // }
       const newBill = new NewBill({ document, onNavigate: onNavigate, store: mockStore, localStorage: window.localStorage })
+      const inputDepense = screen.getByTestId("expense-type");
+      fireEvent.change(inputDepense, { target: { value: "Transports" } });
+
+      const inputName = screen.getByTestId("expense-name");
+      fireEvent.change(inputName, { target: { value: "Bus" } });
+
+
+      const inputDate = screen.getByTestId("datepicker");
+      fireEvent.change(inputDate, { target: { value: "2022-01-01" } });
+
+      const inputMontant = screen.getByTestId("amount");
+      fireEvent.change(inputMontant, { target: { value: "100" } });
+
+
+      const inputComment = screen.getByTestId("vat");
+      fireEvent.change(inputComment, { target: { value: "10" } });
+
+      const inputPct = screen.getByTestId("pct");
+      fireEvent.change(inputPct, { target: { value: "10" } });
+
+
+      const inputAuthor = screen.getByTestId("commentary");
+      fireEvent.change(inputAuthor, { target: { value: "test" } });
+
+
+      const inputFile = screen.getByTestId("file");
+      fireEvent.change(inputFile, {
+        target: {
+          //exemple lunette donné par la documentation 
+          files: [new File(['(⌐□_□)'], 'chucknorris.png', {type: 'image/png'})],
+        },
+      });
+
+
       const handleSubmit = jest.fn(newBill.handleSubmit)
+      newBill.updateBill = jest.fn()
       const form = screen.getByTestId('form-new-bill')
       form.addEventListener('submit', handleSubmit)
       fireEvent.submit(form)
       expect(handleSubmit).toHaveBeenCalled()
+      expect(newBill.updateBill).toHaveBeenCalled()
+
+      const titreDeListe = screen.getByText('Mes notes de frais') //recupere le titre de la page
+      expect(titreDeListe).toBeTruthy()
     })
+
+    
   })
 
