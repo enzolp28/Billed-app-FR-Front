08a3e77b8d385d1cb1ed59dcc0ee28f03d9e82db
enzolp28 <laposta.enzo@gmail.com>
2024-08-30 11:31:35 +0200

    test newBill.js  handleSubmit()

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -10,6 +10,7 @@ import mockStore from "../__mocks__/store.js";
 import userEvent from "@testing-library/user-event";
 import router from "../app/Router.js";
 import { fireEvent } from "@testing-library/dom";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -33,6 +34,25 @@ describe("Given I am connected as an employee", () => {
         },
       })
       expect(handleClickFileButton).toHaveBeenCalled()
+    })
+  })
+
+  describe("When I am on NewBill Page and I click on 'Envoyer'", () => {
+    test("Then I should be sent on Dashboard with big billed icon instead of form", () => {
+      document.body.innerHTML = NewBillUI()
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const newBill = new NewBill({ document, onNavigate: onNavigate, store: mockStore, localStorage: window.localStorage })
+      const handleSubmit = jest.fn(newBill.handleSubmit)
+      const form = screen.getByTestId('form-new-bill')
+      form.addEventListener('submit', handleSubmit)
+      fireEvent.submit(form)
+      expect(handleSubmit).toHaveBeenCalled()
     })
   })
 })
